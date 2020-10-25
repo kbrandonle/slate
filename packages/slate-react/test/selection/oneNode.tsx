10d9ab391069f89d0fc6682c78e444f3e54b84a0
Isaac Schultz <Isaac.Schultz98@gmail.com>
2020-10-24 18:29:23 -0700

    beginning testing, need a mocking library

---
+++ packages/slate-react/test/selection/oneNode.tsx
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+
+import { ReactEditor } from 'slate-react'
+import { jsx } from '../..'
+
+export const input = (
+  <editor>
+    <block>one</block>
+    <block>two</block>
+    <block>three</block>
+  </editor>
+)
+
+export const test = editor => {
+  return ReactEditor.toSlateRange(editor, {
+    anchor: { path: [0, 0], offset: 1 },
+    focus: { path: [1, 0], offset: 2 },
+  })
+}
+
+export const output = { path: [1, 0], offset: 3 }
