e7dd47166dee97ce984b76dea97d8f44e5f908bc
Kenzo Banaag <kenzobanaag@ymail.com>
2020-11-21 14:19:20 -0800

    TEST: Added another test for click

--- packages/slate-react/test/click-event/mention-item-click.tsx
+++ packages/slate-react/test/click-event/mention-item-click.tsx
@@ -1,4 +1,10 @@
+export const input = [
+  {
+    type: 'mention',
+    character: 'Mace Windu',
+    children: [{ text: '' }],
+  },]
 
 export const selector = "span"
 
-export const output = {anchor: {offset: 0, path: [0, 0]}, focus: {offset: 0, path: [0, 0]}} // mention items should also be selected when clicked
+export const output = null
