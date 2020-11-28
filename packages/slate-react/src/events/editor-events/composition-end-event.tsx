b0e33ae1be01afecc6ea4e2f81e124852496f958
Kenzo Banaag <kenzobanaag@ymail.com>
2020-11-28 15:10:01 -0800

    REFACTOR: Structured editor events into a single file

---
+++ packages/slate-react/src/events/editor-events/composition-end-event.tsx
@@ -0,0 +1,24 @@
+import {
+    Editor,
+} from 'slate'
+
+export function handleCompositionEndEvent(props : any) {
+
+    const event : React.CompositionEvent<HTMLDivElement> = props.event
+    const editor = props.editor
+    
+    if (
+        props.hasEditableTarget(editor, event.target) &&
+        !props.isEventHandled(event, props.attributes.onCompositionEnd)
+      ) {
+        props.state.isComposing = false
+
+        // COMPAT: In Chrome, `beforeinput` events for compositions
+        // aren't correct and never fire the "insertFromComposition"
+        // type that we need. So instead, insert whenever a composition
+        // ends since it will already have been committed to the DOM.
+        if (!props.IS_SAFARI && !props.IS_FIREFOX && event.data) {
+          Editor.insertText(editor, event.data)
+        }
+      }
+}
---
+++ packages/slate-react/src/events/editor-events/focus-event.tsx
@@ -0,0 +1,27 @@
+import { ReactEditor } from 'slate-react/src/plugin/react-editor'
+
+export function handleOnFocusEvent(props : any) {
+    
+    const event : React.FocusEvent<HTMLDivElement> = props.event
+    const editor = props.editor
+
+    if (
+        !props.readOnly &&
+        !props.state.isUpdatingSelection &&
+        props.hasEditableTarget(editor, event.target) &&
+        !props.isEventHandled(event, props.attributes.onFocus)
+      ) {
+        const el = ReactEditor.toDOMNode(editor, editor)
+        props.state.latestElement = window.document.activeElement
+
+        // COMPAT: If the editor has nested editable elements, the focus
+        // can go to them. In Firefox, this must be prevented because it
+        // results in issues with keyboard navigation. (2017/03/30)
+        if (props.IS_FIREFOX && event.target !== el) {
+          el.focus()
+          return
+        }
+
+        props.IS_FOCUSED.set(editor, true)
+      }
+}
