8ddf2b1512aec4d1189c51b029da9b542630bbfa
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 14:11:16 -0800

    added testing for deletion - forward and backward

---
+++ packages/slate-react/test/key-event/delete-hotkey-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {ctrlKey : false ,bubbles: true, key : 'delete'})
+
+export const output = [false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
