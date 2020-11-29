9d695c587221dfbb5fc47f517bf67c7478df7061
Kenzo Banaag <kenzobanaag@ymail.com>
2020-11-28 17:48:49 -0800

    TEST: Test for moving forward hotkey

---
+++ packages/slate-react/test/key-event/right-arrow-event.tsx
@@ -0,0 +1,5 @@
+
+export const event = new KeyboardEvent('keypress', {ctrlKey : false ,bubbles: true, key : 'ArrowRight'})
+
+export const output = [false, false, true, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
