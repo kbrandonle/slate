e6b9ca65d74dd399d6237820dbc891dd7933ff3e
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 14:24:48 -0800

    added tests for extension hotkey functionality

---
+++ packages/slate-react/test/key-event/shift+right-hotkey-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {shiftKey : true ,bubbles: true, key : 'ArrowRight'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, 
+    false, false, false]
