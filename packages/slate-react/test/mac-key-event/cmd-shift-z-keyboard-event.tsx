607ceb426c5ff28c6d4190cdc820be6693cf7568
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 15:17:49 -0800

    tested redo functionality for mac

---
+++ packages/slate-react/test/mac-key-event/cmd-shift-z-keyboard-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {shiftKey : true ,metaKey: true, key : 'z'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
