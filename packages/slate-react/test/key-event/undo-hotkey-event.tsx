610a3e1f65085a3cf3014654b7de06b68924d83a
Kenzo Banaag <kenzobanaag@ymail.com>
2020-11-28 17:49:14 -0800

    TEST: Testing undo hotkey

---
+++ packages/slate-react/test/key-event/undo-hotkey-event.tsx
@@ -0,0 +1,5 @@
+
+export const event = new KeyboardEvent('keypress', {ctrlKey : true ,bubbles: true, key : 'z'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, true]
