f2c3485100c99e52b67912b9b9dff5054d3e4f8f
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 17:06:45 -0800

    added tests for extending line selection for mac

---
+++ packages/slate-react/test/mac-key-event/opt-shift-down-hotkey-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {altKey : true , shiftKey: true, key : 'ArrowDown'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
