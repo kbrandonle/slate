4e9572c165ee49ff3d027f6227eb12ae35ccdc5b
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 17:08:59 -0800

    added tests for undo and transpose hotkeys for mac

---
+++ packages/slate-react/test/mac-key-event/ctrl-t-hotkey-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {ctrlKey : true ,bubbles: true, key : 't'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
