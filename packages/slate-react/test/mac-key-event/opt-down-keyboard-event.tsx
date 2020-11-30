bf347a16741cdb31c715932f906c3d63e6623d85
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 16:36:52 -0800

    added tests for move line mac keyboard shortcuts

---
+++ packages/slate-react/test/mac-key-event/opt-down-keyboard-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {altKey : true ,bubbles: true, key : 'ArrowDown'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
