132bbae8a931a446dd25bb1feca3c9d2d49f5879
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 17:09:21 -0800

    added test for split block hotkey

---
+++ packages/slate-react/test/key-event/shift+enter-hotkey-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {shiftKey : true ,bubbles: true, key : 'Enter'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    true, false, false]
