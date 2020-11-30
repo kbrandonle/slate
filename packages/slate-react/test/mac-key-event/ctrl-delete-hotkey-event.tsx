090238f7b22491eb573a83a98f45f52ceae4e7e2
kbrandonle <39102475+kbrandonle@users.noreply.github.com>
2020-11-29 17:04:04 -0800

    added  tests for deleting forwards for mac

---
+++ packages/slate-react/test/mac-key-event/ctrl-delete-hotkey-event.tsx
@@ -0,0 +1,4 @@
+export const event = new KeyboardEvent('keypress', {metaKey : true ,bubbles: true, key : 'delete'})
+
+export const output = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 
+    false, false, false]
